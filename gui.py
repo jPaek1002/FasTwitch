42c08cd20da503c6264874be212056a96efea754
Jay Paek <46653581+jPaek1002@users.noreply.github.com>
2022-05-26 10:23:38 -0700

    fixed angles

--- gui.py
+++ gui.py
@@ -5,11 +5,11 @@ from PIL import ImageTk, Image
 import time
 import numpy as np
 import mediapipe as mp
+from math import floor
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5)
-
 def get_pose(frame):
     global mp_pose
     global pose
@@ -19,20 +19,22 @@ def get_pose(frame):
     lm = results.pose_landmarks
     lmPose = mp_pose.PoseLandmark
     mp_drawing.draw_landmarks(frame,lm, mp_pose.POSE_CONNECTIONS)
-    left_thigh = np.sqrt(np.square(lm.landmark[lmPose.LEFT_HIP].x)+np.square(lm.landmark[lmPose.LEFT_KNEE].x))
-    right_thigh = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_HIP].x)+np.square(lm.landmark[lmPose.RIGHT_KNEE].x))
-    left_calf = np.sqrt(np.square(lm.landmark[lmPose.LEFT_ANKLE].x)+np.square(lm.landmark[lmPose.LEFT_KNEE].x))
-    right_calf = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_ANKLE].x)+np.square(lm.landmark[lmPose.RIGHT_KNEE].x))
-    left_leg = np.sqrt(np.square(lm.landmark[lmPose.LEFT_ANKLE].x)+np.square(lm.landmark[lmPose.LEFT_HIP].x))
-    right_leg = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_ANKLE].x)+np.square(lm.landmark[lmPose.RIGHT_HIP].x))
-    left_angle = np.arccos(np.square(left_thigh)+np.square(left_calf)-np.square(left_leg)/(2*left_thigh*left_calf))
-    right_angle = np.arccos(np.square(right_thigh)+np.square(right_calf)-np.square(right_leg)/(2*right_thigh*right_calf))
-    left_angle = min([left_angle, 360 - left_angle])
-    left_angle = min([right_angle, 360 - right_angle])
+    if lm is not None:
+        left_thigh = np.sqrt(np.square(lm.landmark[lmPose.LEFT_HIP].x)+np.square(lm.landmark[lmPose.LEFT_KNEE].x))
+        right_thigh = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_HIP].x)+np.square(lm.landmark[lmPose.RIGHT_KNEE].x))
+        left_calf = np.sqrt(np.square(lm.landmark[lmPose.LEFT_ANKLE].x)+np.square(lm.landmark[lmPose.LEFT_KNEE].x))
+        right_calf = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_ANKLE].x)+np.square(lm.landmark[lmPose.RIGHT_KNEE].x))
+        left_leg = np.sqrt(np.square(lm.landmark[lmPose.LEFT_ANKLE].x)+np.square(lm.landmark[lmPose.LEFT_HIP].x))
+        right_leg = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_ANKLE].x)+np.square(lm.landmark[lmPose.RIGHT_HIP].x))
+        left_angle = np.square(left_thigh)+np.square(left_calf)-np.square(left_leg)/(2*left_thigh*left_calf)
+        left_angle = left_angle - floor(left_angle)
+        right_angle = np.square(right_thigh)+np.square(right_calf)-np.square(right_leg)/(2*right_thigh*right_calf)
+        right_angle = right_angle - floor(right_angle)
+        left = min([left_angle, 360 - left_angle])
+        right = min([right_angle, 360 - right_angle])
 
 
-
-    return (frame,"Null")
+    return (frame,"Null", left, right)
 
 LARGEFONT = ("Verdana", 35)
 
@@ -75,19 +77,18 @@ class Home(tk.Frame):
                              command=lambda: controller.show_frame(Analysis))
         button2.grid(row=2, column=1, padx=10, pady=10)
 
-class Camera(tk.Frame):
 
+class Camera(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = ttk.Label(self, text="Camera", font=LARGEFONT)
         label.grid(row=0, column=4, padx=10, pady=10)
-
         pose_position = ttk.Label(self, text="Callibrating...", font=LARGEFONT)
         pose_position.grid(row=0, column=0, padx=10, pady=10)
         lmain = ttk.Label(self)
         lmain.grid()
-
         cap = cv2.VideoCapture(0)
+        self.hold = 0
 
         def video_stream():
             _, frame = cap.read()
@@ -102,6 +103,9 @@ class Camera(tk.Frame):
             imgtk = ImageTk.PhotoImage(image=img)
             lmain.imgtk = imgtk
             lmain.configure(image=imgtk)
+            self.hold = self.hold + 1
+            if(self.hold == 100):
+                doSomething()
             lmain.after(12, video_stream)
 
         button1 = ttk.Button(self, text="Home",
