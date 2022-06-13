821630993b8bed800077834fc22c8c68adb98a0f
Jay Paek <46653581+jPaek1002@users.noreply.github.com>
2022-06-13 12:27:08 +0900

    fixed some math

--- gui.py
+++ gui.py
@@ -18,38 +18,44 @@ from matplotlib.backends.backend_tkagg import (
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-pose = mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5)
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+
 def get_pose(frame):
     global mp_pose
     global pose
-    #RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    RGB=frame
+    # RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    RGB = frame
     results = pose.process(RGB)
     lm = results.pose_landmarks
     lmPose = mp_pose.PoseLandmark
-    mp_drawing.draw_landmarks(frame,lm, mp_pose.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(frame, lm, mp_pose.POSE_CONNECTIONS)
     if lm is not None:
-        left_thigh = np.sqrt(np.square(lm.landmark[lmPose.LEFT_HIP].x-lm.landmark[lmPose.LEFT_KNEE].x)+
-                             np.square(lm.landmark[lmPose.LEFT_HIP].y-lm.landmark[lmPose.LEFT_KNEE].y))
+        left_thigh = np.sqrt(np.square(lm.landmark[lmPose.LEFT_HIP].x - lm.landmark[lmPose.LEFT_KNEE].x) +
+                             np.square(lm.landmark[lmPose.LEFT_HIP].y - lm.landmark[lmPose.LEFT_KNEE].y))
         right_thigh = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_HIP].x - lm.landmark[lmPose.RIGHT_KNEE].x) +
-                             np.square(lm.landmark[lmPose.RIGHT_HIP].y - lm.landmark[lmPose.RIGHT_KNEE].y))
+                              np.square(lm.landmark[lmPose.RIGHT_HIP].y - lm.landmark[lmPose.RIGHT_KNEE].y))
         left_calf = np.sqrt(np.square(lm.landmark[lmPose.LEFT_ANKLE].x - lm.landmark[lmPose.LEFT_KNEE].x) +
-                             np.square(lm.landmark[lmPose.LEFT_ANKLE].y - lm.landmark[lmPose.LEFT_KNEE].y))
+                            np.square(lm.landmark[lmPose.LEFT_ANKLE].y - lm.landmark[lmPose.LEFT_KNEE].y))
         right_calf = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_ANKLE].x - lm.landmark[lmPose.RIGHT_KNEE].x) +
                              np.square(lm.landmark[lmPose.RIGHT_ANKLE].y - lm.landmark[lmPose.RIGHT_KNEE].y))
         left_leg = np.sqrt(np.square(lm.landmark[lmPose.LEFT_ANKLE].x - lm.landmark[lmPose.LEFT_HIP].x) +
-                             np.square(lm.landmark[lmPose.LEFT_ANKLE].y - lm.landmark[lmPose.LEFT_HIP].y))
+                           np.square(lm.landmark[lmPose.LEFT_ANKLE].y - lm.landmark[lmPose.LEFT_HIP].y))
         right_leg = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_ANKLE].x - lm.landmark[lmPose.RIGHT_HIP].x) +
-                             np.square(lm.landmark[lmPose.RIGHT_ANKLE].y - lm.landmark[lmPose.RIGHT_HIP].y))
-        left_angle = np.arccos((np.square(left_thigh)+np.square(left_calf)-np.square(left_leg))/(2*left_thigh*left_calf))
-        right_angle = np.arccos((np.square(right_thigh)+np.square(right_calf)-np.square(right_leg))/(2*right_thigh*right_calf))
+                            np.square(lm.landmark[lmPose.RIGHT_ANKLE].y - lm.landmark[lmPose.RIGHT_HIP].y))
+        left_angle = np.arccos(
+            (np.square(left_thigh) + np.square(left_calf) - np.square(left_leg)) / (2 * left_thigh * left_calf))
+        right_angle = np.arccos(
+            (np.square(right_thigh) + np.square(right_calf) - np.square(right_leg)) / (2 * right_thigh * right_calf))
         left = min([left_angle, 2 * np.pi - left_angle])
         right = min([right_angle, 2 * np.pi - right_angle])
         # print(left, right)
         return (frame, "Null", left, right)
     return None
 
+
 LARGEFONT = ("Verdana", 35)
+
 
 class tkinterApp(tk.Tk):
 
@@ -94,7 +100,6 @@ class Home(tk.Frame):
         button2.grid(row=2, column=1, padx=10, pady=10)
 
 
-
 class Camera(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -106,21 +111,23 @@ class Camera(tk.Frame):
         lmain.grid()
         cap = cv2.VideoCapture(0)
         self.hold = 0
-        self.fps =  cap.get(cv2.CAP_PROP_FPS)
+        self.fps = cap.get(cv2.CAP_PROP_FPS)
         self.thread = threading.Thread()
         self.angles = []
         self.delay = 0
         self.controller = controller
+
         def ready(delay):
             playsound()
             self.thread.join()
+
         def video_stream():
             _, frame = cap.read()
-            #proess frame here
+            # proess frame here
             cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            for i in range(0,len(cv2image)):
-                cv2image[i]=cv2image[i][::-1]
-            #cv2image = cv2image[::-1]
+            for i in range(0, len(cv2image)):
+                cv2image[i] = cv2image[i][::-1]
+            # cv2image = cv2image[::-1]
             tup = get_pose(cv2image)
             if tup is not None:
                 pose_position.configure(text=tup[1]);
@@ -128,18 +135,19 @@ class Camera(tk.Frame):
                 imgtk = ImageTk.PhotoImage(image=img)
                 lmain.imgtk = imgtk
                 lmain.configure(image=imgtk)
-                if tup[2]<100 or tup[3]<100 and self.hold >= 0:
+                if tup[2] < 100 or tup[3] < 100 and self.hold >= 0:
                     self.hold = self.hold + 1
-                if self.hold == 1.3*self.fps:
-                    self.delay = random.uniform(0,1)
+                if self.hold == 1.3 * self.fps:
+                    self.delay = random.uniform(0, 1)
                     self.thread.start(ready(self.delay))
                     self.hold == -1
                 if self.hold < 0:
-                    self.angles.append(max(tup[2],tup[3])/2)
+                    self.angles.append(max(tup[2], tup[3]) / 2)
                     self.hold = self.hold - 1
-                if self.hold == -5*self.fps:
+                if self.hold == -5 * self.fps:
                     self.hold = 0
                     self.controller.get_page('Analysis').set_angles(self.angles)
+                    self.controller.get_page('Analysis').set_delay(self.delay)
 
             lmain.after(12, video_stream)
 
@@ -155,10 +163,12 @@ class Camera(tk.Frame):
 
         video_stream()
 
+
 class Analysis(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.angles = []
+        self.delay = 0
         label = ttk.Label(self, text="Analysis", font=LARGEFONT)
         label.pack()
         figure = Figure(figsize=(6, 4), dpi=100)
@@ -171,22 +181,19 @@ class Analysis(tk.Frame):
 
         button1 = ttk.Button(self, text="Camera",
                              command=lambda: controller.show_frame(Camera))
-
         button1.pack()
-
         button2 = ttk.Button(self, text="Home",
                              command=lambda: controller.show_frame(Home))
-
         button2.pack()
 
-
-    def get_angles(self, angles):
+    def set_values(self, angles):
         self.angles = angles
+
+    def set_delay(self, delay):
+        self.delay = delay
+
 
 time.sleep(0.1)
 app = tkinterApp()
 app.title("FasTwitch")
 app.mainloop()
-
-
-
