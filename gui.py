0208eea23a95641cc0918d640ae22af88e01acc0
Jay Paek <46653581+jPaek1002@users.noreply.github.com>
2022-08-16 15:42:13 -0700

    more mode

--- gui.py
+++ gui.py
@@ -50,12 +50,35 @@ def get_pose(frame, mode):
         left = min([left_angle, 2 * np.pi - left_angle])
         right = min([right_angle, 2 * np.pi - right_angle])
         # print(left, right)
-        return (frame, "Null", left, right)
+        return (frame, left, right)
+    if lm is not None and mode == 1:
+        left_bicep = np.sqrt(np.square(lm.landmark[lmPose.LEFT_SHOULDER].x - lm.landmark[lmPose.LEFT_ELBOW].x) +
+                             np.square(lm.landmark[lmPose.LEFT_SHOULDER].y - lm.landmark[lmPose.LEFT_ELBOW].y))
+        left_forearm = np.sqrt(np.square(lm.landmark[lmPose.LEFT_WRIST].x - lm.landmark[lmPose.LEFT_ELBOW].x) +
+                            np.square(lm.landmark[lmPose.LEFT_WRIST].y - lm.landmark[lmPose.LEFT_ELBOW].y))
+        left_arm = np.sqrt(np.square(lm.landmark[lmPose.LEFT_WRIST].x - lm.landmark[lmPose.LEFT_SHOULDER].x) +
+                           np.square(lm.landmark[lmPose.LEFT_WRIST].y - lm.landmark[lmPose.LEFT_SHOULDER].y))
+        left_angle = np.arccos(
+            (np.square(left_bicep) + np.square(left_forearm) - np.square(left_arm)) / (2 * left_bicep * left_forearm))
+        left = min([left_angle, 2 * np.pi - left_angle])
+        # print(left, right)
+        return (frame, left)
+    if lm is not None and mode == 2:
+        right_bicep = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_SHOULDER].x - lm.landmark[lmPose.RIGHT_ELBOW].x) +
+                              np.square(lm.landmark[lmPose.RIGHT_SHOULDER].y - lm.landmark[lmPose.RIGHT_ELBOW].y))
+        right_forearm = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_WRIST].x - lm.landmark[lmPose.RIGHT_ELBOW].x) +
+                             np.square(lm.landmark[lmPose.RIGHT_WRIST].y - lm.landmark[lmPose.RIGHT_ELBOW].y))
+        right_arm = np.sqrt(np.square(lm.landmark[lmPose.RIGHT_WRIST].x - lm.landmark[lmPose.RIGHT_SHOULDER].x) +
+                            np.square(lm.landmark[lmPose.RIGHT_WRIST].y - lm.landmark[lmPose.RIGHT_SHOULDER].y))
+        right_angle = np.arccos(
+            ((np.square(right_bicep) + np.square(right_forearm) - np.square(right_arm)) / (2 * right_bicep * right_forearm)))
+        right = min([right_angle, 2 * np.pi - right_angle])
+        # print(left, right)
+        return (frame, right)
     return None
 
 
 LARGEFONT = ("Verdana", 35)
-
 
 class tkinterApp(tk.Tk):
 
@@ -117,6 +140,8 @@ class Camera(tk.Frame):
         self.delay = 0
         self.controller = controller
         self.mode = 0
+        self.reps = 0
+        self.lowering = False
 
         def ready(delay):
             playsound()
@@ -124,6 +149,8 @@ class Camera(tk.Frame):
 
         def set_mode(mode):
             self.mode = mode
+            self.reps = 0
+            print(self.mode)
 
         def video_stream():
             _, frame = cap.read()
@@ -134,24 +161,38 @@ class Camera(tk.Frame):
             # cv2image = cv2image[::-1]
             tup = get_pose(cv2image, self.mode)
             if tup is not None:
-                pose_position.configure(text=tup[1]);
+                pose_position.configure(text=self.reps)
                 img = Image.fromarray(tup[0])
                 imgtk = ImageTk.PhotoImage(image=img)
                 lmain.imgtk = imgtk
                 lmain.configure(image=imgtk)
-                if tup[2] < 100 or tup[3] < 100 and self.hold >= 0:
-                    self.hold = self.hold + 1
-                if self.hold == 1.3 * self.fps:
-                    self.delay = random.uniform(0, 1)
-                    self.thread.start(ready(self.delay))
-                    self.hold == -1
-                if self.hold < 0:
-                    self.angles.append(max(tup[2], tup[3]) / 2)
-                    self.hold = self.hold - 1
-                if self.hold == -5 * self.fps:
-                    self.hold = 0
-                    self.controller.get_page('Analysis').set_angles(self.angles)
-                    self.controller.get_page('Analysis').set_delay(self.delay)
+                if self.mode == 0:
+                    if tup[1] < 5*np.pi/9 or tup[2] < 5*np.pi/9 and self.hold >= 0:
+                        self.hold = self.hold + 1
+                    if self.hold == 1.3 * self.fps:
+                        self.delay = random.uniform(0, 1)
+                        self.thread.start(ready(self.delay))
+                        self.hold == -1
+                    if self.hold < 0:
+                        self.angles.append(max(tup[1], tup[2]) / 2)
+                        self.hold = self.hold - 1
+                    if self.hold == -5 * self.fps:
+                        self.hold = 0
+                        self.controller.get_page('Analysis').set_angles(self.angles)
+                        self.controller.get_page('Analysis').set_delay(self.delay)
+                elif self.mode == 1:
+                    if self.lowering and tup[1] > np.pi-np.pi/9:
+                        self.reps = self.reps + 1
+                        self.lowering = False
+                    elif tup[1] < np.pi/4:
+                        self.lowering = True
+                elif self.mode == 2:
+                    print(tup[1])
+                    if self.lowering and tup[1] > np.pi-np.pi/9:
+                        self.reps = self.reps + 1
+                        self.lowering = False
+                    elif tup[1] < np.pi/4:
+                        self.lowering = True
 
             lmain.after(12, video_stream)
 
@@ -166,7 +207,7 @@ class Camera(tk.Frame):
         button2.grid(row=2, column=1, padx=10, pady=10)
 
         button3 = ttk.Button(self, text="Change Mode",
-                             command=lambda: set_mode((self.mode+1)%2))
+                             command=lambda: set_mode((self.mode+1)%3))
 
         button3.grid(row=2, column=1, padx=10, pady=10)
 
